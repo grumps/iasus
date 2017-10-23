229ad87411fe0d2ea303a86318cc15e2188b9754
grumps <max@ofmax.li>
2017-10-22 22:32:46 -0700

    working lil rest endpoint

--- iasus/iasus.py
+++ iasus/iasus.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+import json
+
+import falcon
+
+
+class ImageMetaResource(object):
+
+    def on_post(self, req, resp, uid):
+        resp.status = falcon.HTTP_201
+        data = json.load(req.bounded_stream)
+        resp.status = falcon.HTTP_201
+
+
+api = application = falcon.API()
+api.add_route('/api/v1/file/{uid}/meta/', ImageMetaResource())
